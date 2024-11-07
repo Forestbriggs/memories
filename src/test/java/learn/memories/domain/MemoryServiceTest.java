29db902fd48660753caa24c01c998c9b0794dc6c
Ocean Briggs-Mineart <Forestbriggs14@gmail.com>
2024-11-07 15:55:42 -0800

    created tests for MemoryService

--- src/test/java/learn/memories/domain/MemoryServiceTest.java
+++ src/test/java/learn/memories/domain/MemoryServiceTest.java
@@ -1,31 +1,107 @@
 package learn.memories.domain;
 
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import learn.memories.data.DataAccessException;
+import learn.memories.data.MemoryRepositoryDouble;
+import learn.memories.models.Memory;
 
 public class MemoryServiceTest {
 
-    @Test
-    void testAdd() {
+    MemoryService service;
 
+    @BeforeEach
+    void setup() {
+        MemoryRepositoryDouble repository = new MemoryRepositoryDouble();
+        service = new MemoryService(repository);
     }
 
     @Test
-    void testDeleteById() {
-
+    void shouldFindTwoPublicMemories() throws DataAccessException {
+        List<Memory> memories = service.findPublicMemories();
+        assertEquals(2, memories.size());
     }
 
     @Test
-    void testFindPrivateMemories() {
-
+    void shouldFindOnePrivateMemory() throws DataAccessException {
+        List<Memory> memories = service.findPrivateMemories();
+        assertEquals(1, memories.size());
     }
 
     @Test
-    void testFindPublicMemories() {
+    void shouldNotAddNullFrom() throws DataAccessException {
+        // Arrange
+        Memory memory = new Memory();
+        memory.setContent("That one time we...");
 
+        // Act
+        MemoryResult result = service.add(memory);
+
+        // Assert
+        assertFalse(result.isSuccess());
+        assertEquals(1, result.getErrorMessages().size());
+        assertTrue(result.getErrorMessages().get(0).contains("`from`"));
     }
 
     @Test
-    void testUpdate() {
+    void shouldNotAddEmptyContent() throws DataAccessException {
+        Memory memory = new Memory();
+        memory.setFrom("Zonda Itscowics");
 
+        MemoryResult result = service.add(memory);
+
+        assertFalse(result.isSuccess());
+        assertEquals(1, result.getErrorMessages().size());
+        assertTrue(result.getErrorMessages().get(0).contains("`content`"));
+    }
+
+    @Test
+    void shouldNotAddPositiveId() throws DataAccessException {
+        Memory memory = new Memory(12, "from", "content", true);
+
+        MemoryResult result = service.add(memory);
+
+        assertFalse(result.isSuccess());
+        assertEquals(1, result.getErrorMessages().size());
+        assertTrue(result.getErrorMessages().get(0).contains("`id`"));
+    }
+
+    @Test
+    void shouldAdd() throws DataAccessException {
+        Memory memory = new Memory();
+        memory.setFrom("Zonda Itscowics");
+        memory.setContent("That one time we...");
+
+        MemoryResult result = service.add(memory);
+
+        assertTrue(result.isSuccess());
+    }
+
+    @Test
+    void shouldNotUpdateEmptyContent() throws DataAccessException {
+        Memory memory = service.findPublicMemories().get(0);
+        memory.setContent("\t\n ");
+
+        MemoryResult result = service.update(memory);
+
+        assertFalse(result.isSuccess());
+        assertEquals(1, result.getErrorMessages().size());
+        assertTrue(result.getErrorMessages().get(0).contains("`content`"));
+    }
+
+    @Test
+    void shouldUpdate() throws DataAccessException {
+        Memory memory = service.findPublicMemories().get(0);
+        memory.setContent("updated content");
+
+        MemoryResult result = service.update(memory);
+
+        assertTrue(result.isSuccess());
     }
 }
