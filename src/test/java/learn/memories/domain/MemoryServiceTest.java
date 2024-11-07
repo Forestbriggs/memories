9a936990b6995cbfa531758479202d914c8938b4
Ocean Briggs-Mineart <Forestbriggs14@gmail.com>
2024-11-07 15:53:06 -0800

    started making MemoryServiceTest, made MemoryRepositoryDouble to mock MemoryRepository

---
+++ src/test/java/learn/memories/domain/MemoryServiceTest.java
@@ -0,0 +1,31 @@
+package learn.memories.domain;
+
+import org.junit.jupiter.api.Test;
+
+public class MemoryServiceTest {
+
+    @Test
+    void testAdd() {
+
+    }
+
+    @Test
+    void testDeleteById() {
+
+    }
+
+    @Test
+    void testFindPrivateMemories() {
+
+    }
+
+    @Test
+    void testFindPublicMemories() {
+
+    }
+
+    @Test
+    void testUpdate() {
+
+    }
+}
