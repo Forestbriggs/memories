34f54465fdba3870b9b79f399c634c95b5200c94
Ocean Briggs-Mineart <Forestbriggs14@gmail.com>
2024-11-12 15:03:13 -0800

    fixed beans and refactored App.java

--- src/main/java/learn/memories/App.java
+++ src/main/java/learn/memories/App.java
@@ -1,26 +1,16 @@
 package learn.memories;
 
-import learn.memories.data.MemoryFileRepository;
-import learn.memories.domain.MemoryService;
-import learn.memories.ui.ConsoleIO;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 import learn.memories.ui.Controller;
-import learn.memories.ui.View;
 
 public class App {
 
     public static void main(String[] args) {
-        // "./data/memories.txt" is the path to the "production" data file.
-        MemoryFileRepository repository = new MemoryFileRepository("./data/memories.txt");
-        // repository satisfies the MemoryRepository dependency.
-        MemoryService service = new MemoryService(repository);
+        ApplicationContext context = new ClassPathXmlApplicationContext("dependency-configuration.xml");
 
-        ConsoleIO io = new ConsoleIO();
-        // io satisfies the TextIO dependency.
-        View view = new View(io);
-
-        // view satisfies the View dependency
-        // service satisfies the MemoryService dependency
-        Controller controller = new Controller(view, service);
+        Controller controller = context.getBean(Controller.class);
 
         // And so it begins
         controller.run();
